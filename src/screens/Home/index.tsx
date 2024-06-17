6f509969045c57f965344af745ddb4d30dc6d96f
joaogkvalho <joaogkvalho@gmail.com>
2024-06-17 14:44:19 -0300

    :tada: project created and initiated

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import { StatusBar, Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.eventName}>
+          Nome do evento
+        </Text>
+
+        <Text style={styles.eventDate}>
+          Sexta, 4 de novembro de 2022.
+        </Text>
+      </View>
+    </>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      gap: 4,
+      backgroundColor: "#131016",
+    },
+    eventName: {
+      fontSize: 22,
+      fontWeight: "bold",
+      marginTop: 28,
+      color: "#fdfcfe"
+    },
+    eventDate: {
+      color: "#6b6b6b",
+      fontSize: 14,
+    }
+})
